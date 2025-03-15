d533f27ff55d3a44f473593634d066f33b93465c
Kyle <77381061+Don330@users.noreply.github.com>
2025-03-15 13:20:08 +1100

    Update database.py
    Change password

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 #from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Blueflag1!@localhost/task_manager"
+DATABASE_URL = "mysql+pymysql://root:<password@localhost/task_manager"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
